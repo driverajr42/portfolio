1046badb49f0a5918fdb70e2e6eea7e5bec9644f
Dennis <driverajr127@gmail.com>
2022-09-10 08:20:39 -0400

    added tailwind and updated config file for next

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        header: "url('public/headerBg.svg')"
+      },
+      colors: {
+        'primary': '#0A192F',
+        'secondary': '#3C978D',
+        'mainTxt': '#8892B0',
+        'accent': '#CCD6F6'
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1600px',
+      }
+    },
   },
   plugins: [],
 }

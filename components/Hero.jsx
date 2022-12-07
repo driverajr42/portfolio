a9ddbbe4ee3208e26e1260d149bfff8b2189e7d4
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-07 15:55:07 -0800

    Added featureProject.svg into public folder.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = ({ heading, message }) => {
 		<div className="flex items-center justify-center h-screen bg-fix bg-center bg-cover hero-img">
 			<div className="p-5 text-center z-[2] mt-[-20rem] md:p-56 lg:p-96 xl:p-[38rem]">
 				<h2 className="text-5xl font-bold text-accentTwo">{heading}</h2>
-				<p className="py-5 text-xl text-mainTxt">{message}</p>
-				<button className="px-8 py-2 bg-secondary rounded-md text-accentTwo">
+				<p className="py-5 text-xl text-accentTwo">{message}</p>
+				<button className="px-8 py-2  border-2 border-secondary rounded-full text-secondary">
 					<Link href="/#portfolio">Explore Work</Link>
 				</button>
 			</div>

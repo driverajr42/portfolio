8ae3b25afacad057b725ceef1fd1f769ebbcf9a1
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-09 06:22:27 -0800

    Added margin bottom to content in hero. Added primary bg to entire site.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,17 +7,17 @@ import featureImg from '../public/featureProject.svg';
 const Hero = ({ heading, message }) => {
 	return (
 		<div className="flex items-center justify-center h-screen bg-fix bg-center bg-cover hero-img">
-			<div className="p-5 text-center z-[2] mt-[-20rem] md:p-56 lg:p-96 xl:p-[38rem]">
-				<h2 className="text-5xl font-bold text-accentTwo">{heading}</h2>
-				<p className="py-5 text-xl text-accentTwo">{message}</p>
+			<div className="p-5 text-center z-[2] mt-[-32rem] md:p-56 lg:p-96 xl:p-[38rem]">
+				<h2 className="text-5xl font-bold text-accentTwo mb-8">{heading}</h2>
+				<p className="py-5 text-xl text-accentTwo mb-8">{message}</p>
 				<button className="px-8 py-2  border-2 border-secondary rounded-full text-secondary">
 					<Link href="/#portfolio">Explore Work</Link>
 				</button>
 			</div>
 
 			{/* Featured Project */}
-			<div className="absolute bottom-48">
-				<Image src={featureImg} alt="Something" width={800} height={800} />
+			<div className="absolute bottom-80 ">
+				<Image src={featureImg} alt="Something"  />
 			</div>
 		</div>
 	);

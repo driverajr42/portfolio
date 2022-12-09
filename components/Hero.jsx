43a3c74e37974985727a5916f49932a4f5abd56d
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-09 06:15:14 -0800

    Removed featureProject component from project.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,9 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+
+import featureImg from '../public/featureProject.svg';
+
 const Hero = ({ heading, message }) => {
 	return (
 		<div className="flex items-center justify-center h-screen bg-fix bg-center bg-cover hero-img">
@@ -9,6 +13,11 @@ const Hero = ({ heading, message }) => {
 				<button className="px-8 py-2  border-2 border-secondary rounded-full text-secondary">
 					<Link href="/#portfolio">Explore Work</Link>
 				</button>
+			</div>
+
+			{/* Featured Project */}
+			<div className="absolute bottom-48">
+				<Image src={featureImg} alt="Something" width={800} height={800} />
 			</div>
 		</div>
 	);
--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
 // Exporting all components in this file. Allowing me to use components whenever needed while focusing on writing one line of code for imported components
 
-export { default as Hero } from './Hero'
-export { default as Navbar } from './Navbar'
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';

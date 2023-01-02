51927d51b26cc366ea47e294f76130f1ebae5a32
Jungo <jungo@Jungos-MBP.lan>
2023-01-02 13:47:46 -0500

    Restructured project. Removed tailkwindcss.

--- components/index.js
+++
@@ -1,4 +0,0 @@
-// Exporting all components in this file. Allowing me to use components whenever needed while focusing on writing one line of code for imported components
-
-export { default as Hero } from './Hero';
-export { default as Navbar } from './Navbar';

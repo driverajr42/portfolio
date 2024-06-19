dc00caf4090e3ba93c03f51f7f39d7facb9b6af2
Dennis <106558555+driverajr42@users.noreply.github.com>
2024-06-19 18:36:27 -0400

    Added BentoGrid to util folder and assests. Also added data folder with data for website.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Hero from '../components/Hero'
 import {FloatingNav} from '../components/ui/FloatingNav'
 import { FaHome } from "react-icons/fa";
+import Grid from "../components/Grid";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           ]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

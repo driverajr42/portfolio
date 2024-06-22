c95454a82b73c6a3113fad593464411e0f8958c7
Dennis <106558555+driverajr42@users.noreply.github.com>
2024-06-22 09:18:22 -0400

    Added GridGlobe and Globe in ui folder for github globe animation from Acenternity UI.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import{ GlobeDemo} from "./GridGlobe"
 
 export const BentoGrid = ({
   className,
@@ -11,7 +12,7 @@ export const BentoGrid = ({
   return (
     <div
     className={cn(
-        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto overflow-hidden",
         className
       )}
     >
@@ -85,18 +86,15 @@ export const BentoGridItem = ({
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-        </div>
-        </div>
-     
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
 
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          <div className="font-sans font-bold text-lg lg:text-3xl mx-w-96 z-10">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
         </div>
-      </div>
+
+        {id === 2 && <GlobeDemo />}
+
+        </div>
     </div>
   );
 };
